0de963d83890dd8e0ec1e65ec6e1f11b04f7835a
Luis Joel Caballero <luijo@outlook.com>
2020-02-01 23:51:14 -0600

    App fully working

--- bamazon.js
+++ bamazon.js
@@ -15,6 +15,8 @@ connection.connect(function(err){
     createTable();
 });
 
+
+
 var createTable = function(){
     connection.query("SELECT * FROM products", function(err, res){
         for (var i=0; i<res.length; i++){
@@ -25,3 +27,49 @@ var createTable = function(){
         promptClient(res);
     })
 };
+ 
+var promptClient = function(res){
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "Welcome. Which article would you like to purchase? [Press Q to quit]"
+    }]).then(function(answer){
+        var correct = false;
+        if(answer.choice.toUpperCase()== "Q"){
+            process.exit();
+        }
+        for (var i=0; i<res.length; i++){
+            if(res[i].productName == answer.choice){
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name:"quant",
+                    message:"How many would you like to buy?",
+                    validate: function(value){
+                        if(isNaN(value)== false){
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function(answer){
+                    if((res[id].stockquantity - answer.quant)>0){
+                        connection.query("UPDATE products SET stockquantity='"+(res[id].stockquantity - answer.quant)+"' WHERE productName='"+product+"'", function(err, res2){
+                           console.log("You got the product!");
+                           createTable(); 
+                        })
+                    } else {
+                        console.log("Not a valid selection!");
+                        promptClient(res);
+                    }
+                })
+            }
+        }
+            if(i==res.length && correct==false){
+                console.log("That selection is not valid!");
+                promptClient(res);
+        }
+    })
+}
